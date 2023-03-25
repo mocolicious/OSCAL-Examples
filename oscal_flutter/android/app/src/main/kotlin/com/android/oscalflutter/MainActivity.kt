c805ce1e203fdfb3ecd8edf73f56e29c0b311120
Andrew <andrewmocol@hotmail.com>
2023-03-25 16:35:28 -0500

    update flutter deps

--- oscal_flutter/android/app/src/main/kotlin/com/android/oscalflutter/MainActivity.kt
+++ oscal_flutter/android/app/src/main/kotlin/com/android/oscalflutter/MainActivity.kt
@@ -3,7 +3,6 @@ package com.android.oscalflutter
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
